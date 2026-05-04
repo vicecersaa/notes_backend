7160e1846fddc677acd86fffc3094802df1ff39b
vice <david@spectreinnovations.com>
2026-05-04 15:58:01 +0700

    fix bug

--- routes/Enquiry.js
+++ routes/Enquiry.js
@@ -5,9 +5,7 @@ const nodemailer = require("nodemailer");
 
 // 🔧 Setup SMTP (Hostinger)
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
